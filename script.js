033f84621eb11406ff2e5d7037fae76d81005d21
Vitaliy Hureyev <hbeatprod@gmail.com>
2021-06-29 21:17:15 +0300

    import smoothscroll

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //Get the button
 let toTopButton = document.getElementById("toTopBtn");
-toTopButton.addEventListener("touchstart", true)
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
